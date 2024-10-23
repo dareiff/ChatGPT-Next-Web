953e76f5124710cf4c20359590af9840958dd144
Derek Reiff <d@derekr.net>
2024-10-22 18:45:36 -0700

    add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-
+import PlausibleProvider from "next-plausible";
 import { Home } from "./components/home";
 
 import { getServerSideConfig } from "./config/server";
@@ -9,7 +9,13 @@ const serverConfig = getServerSideConfig();
 export default async function App() {
   return (
     <>
-      <Home />
+      <PlausibleProvider
+        domain="myfrienddereks.com"
+        selfHosted={true}
+        customDomain="https://io.fun-club.xyz"
+      >
+        <Home />
+      </PlausibleProvider>
       {serverConfig?.isVercel && (
         <>
           <Analytics />
